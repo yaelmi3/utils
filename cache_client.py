65752f3f8bd3233588227dd539b1b0996fee0b8a
Yael Mintz <ymintz@infinidat.com>
2018-07-08 18:47:09 +0300

    1. Fix add to cache update ttl method 2. Requirements update 3. Fix timedelta 4. remove pycharm idea 5. add get_from_cache option 6. check if key is found before attempting to return it 7. change return value by cache to netref list

--- cache_client.py
+++ cache_client.py
@@ -3,16 +3,25 @@ import rpyc
 
 
 def connect_to_cache():
-    return rpyc.connect("yaelm-freddy", 6378).root
+    return rpyc.connect("yaelm-freddy", 6378)
 
 
-def update_cache(key_name, object):
+def update_cache(key_name, data, days_to_keep=30):
     """
     1. Serialize given object
     2. Send object to cache server
     3. Raise exception if couldn't add to cache
-    :param key_name:
+    :type key_name: str
+    :type data: object
     """
-    pickled_object = pickle.dumps(object)
+    pickled_object = pickle.dumps(data)
     connection = connect_to_cache()
-    connection.exposed_add_to_cache(key_name, object)
+    connection.root.exposed_add_to_cache(key_name, pickled_object, days_to_keep)
+
+
+def get_from_cache(key_name):
+    connection = connect_to_cache()
+    pickled_object = connection.root.exposed_get_from_cache(key_name)
+    if pickled_object:
+        return pickle.loads(pickled_object)
+
