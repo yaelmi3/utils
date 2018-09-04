2bc4c47433da38e2d40e261e0e92621afdd954ab
Yael Mintz <ymintz@infinidat.com>
2018-09-04 15:01:16 +0300

    update gunicorn script

--- ui/run_server.sh
+++ ui/run_server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+export LOGS_DIR=/var/log/gunicorn
 export PYTHONPATH=$PWD
-gunicorn ui.webui:app -p webui.pid -b 0.0.0.0:8000 --workers=10 -D
+gunicorn ui.webui:app --log-file=${LOGS_DIR}/webui_gunicorn.log --access-logfile=${LOGS_DIR}/access_gunicron.log --error-logfile=${LOGS_DIR}/webui_gunicorn_errors.log --workers 4 --max-requests 50000 --bind=0.0.0.0:8080 --timeout 3600 -D
