9eb9cd533da6d6158ee5d322e3e0554bf3a6ca51
Yael Mintz <ymintz@infinidat.com>
2018-09-04 20:10:59 +0300

    Prepare settings for global cache Run gunicorn with a single worker for now, to allow local cache to work properly, until global cache is implemented

--- ui/run_server.sh
+++ ui/run_server.sh
@@ -5,5 +5,5 @@ export PYTHONPATH=$PWD
 gunicorn ui.webui:app --log-file=${LOGS_DIR}/webui_gunicorn.log \
 --access-logfile=${LOGS_DIR}/webui_access_gunicron.log\
  --error-logfile=${LOGS_DIR}/webui_gunicorn_errors.log\
-  --workers 4 --max-requests 50000 --bind=0.0.0.0:8080 --timeout 3600\
+  --workers 1 --max-requests 50000 --bind=0.0.0.0:8080 --timeout 3600\
    --capture-output --enable-stdio-inheritance -D
