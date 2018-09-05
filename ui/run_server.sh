460820da2494a6ba672c3e983a0f839a573f8032
Yael Mintz <ymintz@infinidat.com>
2018-09-05 11:24:41 +0300

    add gevent to the gunicorn execution and add it to requiremnts

--- ui/run_server.sh
+++ ui/run_server.sh
@@ -2,8 +2,8 @@
 
 export LOGS_DIR=/var/log/gunicorn
 export PYTHONPATH=$PWD
-gunicorn ui.webui:app --log-file=${LOGS_DIR}/webui_gunicorn.log \
+gunicorn ui.webui:app -k gevent --log-file=${LOGS_DIR}/webui_gunicorn.log \
 --access-logfile=${LOGS_DIR}/webui_access_gunicron.log\
  --error-logfile=${LOGS_DIR}/webui_gunicorn_errors.log\
-  --workers 1 --max-requests 50000 --bind=0.0.0.0:8080 --timeout 3600\
+  --workers 100 --max-requests 50000 --bind=0.0.0.0:8080 --timeout 3600\
    --capture-output --enable-stdio-inheritance -D
